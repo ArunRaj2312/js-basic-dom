3d1f924e82ca1517a9e7c48a7bc17db10e57464d
ArunRaj2312 <arunvijay1043534@gmail.com>
2022-12-28 16:39:37 -1200

    add file

---
+++ main.js
@@ -0,0 +1,6 @@
+let a=document.getElementsByTagName("h1");
+console.log(a[0].innerText);
+let b=document.getElementsByClassName("para");
+console.log(b[0].innerText);
+let c=document.getElementsByName("helo");
+console.log(c[0].innerText);
